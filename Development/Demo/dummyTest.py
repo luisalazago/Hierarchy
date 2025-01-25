91b46d30d7c927578ad71727e20cccc8f487df72
luisalazago <luisalazago@gmail.com>
2025-01-25 18:58:54 -0500

    [Fix] demo OCR & [Fix] web UI

--- Development/Demo/dummyTest.py
+++ Development/Demo/dummyTest.py
@@ -2,21 +2,32 @@ import os
 import pytesseract
 from PIL import Image
 
-def readImage():
-    _, _, files = next(os.walk("images"))
-    for i in range(len(files)):
-        image_path = "images/dummypic{}.png".format(i + 1)
-        image = Image.open(image_path)
+def readImage(name, option=0):
+    ans = None
+    if not option:
+        _, _, files = next(os.walk("images"))
+        for i in range(len(files)):
+            image_path = "images/dummypic{}.png".format(i + 1)
+            image = Image.open(image_path)
 
+            extracted_text = pytesseract.image_to_string(image, lang="eng")
+            cleaned_text = extracted_text.strip()
+
+            if not i:
+                file1 = open("images_output.txt", "w")
+            else:
+                file1 = open("images_output.txt", "a")
+            
+            file1.write(cleaned_text)
+            file1.write("\n")
+            file1.write("\n")
+            file1.close()
+    else:
+        image = Image.open("images/" + name)
+        
         extracted_text = pytesseract.image_to_string(image, lang="eng")
         cleaned_text = extracted_text.strip()
-
-        if not i:
-            file1 = open("images_output.txt", "w")
-        else:
-            file1 = open("images_output.txt", "a")
         
-        file1.write(cleaned_text)
-        file1.write("\n")
-        file1.write("\n")
-        file1.close()
+        ans = cleaned_text
+    return ans
+        
--- Development/Demo/main.py
+++ Development/Demo/main.py
@@ -1,8 +1,10 @@
 import os
+from dummyTest import readImage
 from flask import Flask, render_template, url_for, request, flash, redirect, send_from_directory
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = "images"
+msg = None
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
@@ -24,11 +26,26 @@ def home():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             return redirect(url_for("download_file", name=filename))
-    return render_template("index.html")
+    return render_template("index.html", msg="", text="")
 
 @app.route("/uploads/<name>")
 def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+    global msg
+    msg = "The file was uploaded successfully!"
+    send_from_directory(app.config["UPLOAD_FOLDER"], name)
+    return redirect(url_for("show_text", name=name))
+
+@app.route("/<name>", methods=["GET", "POST"])
+def show_text(name):
+    global msg
+    display_msg = ""
+    text = ""
+    if request.method == "GET":
+        if msg != None:
+            display_msg = msg
+            msg = None
+        text = readImage(name, 1)
+    return render_template("index.html", msg=display_msg, text=text)
 
 if __name__ == "__main__":
     app.run(debug=True)
